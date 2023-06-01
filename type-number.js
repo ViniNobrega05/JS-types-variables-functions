2886087eb963b81ca1ce169cf9d3eca96656d694
ViniNobrega05 <vinicius_nobrega05@hotmail.com>
2023-06-01 11:58:03 -0300

    exemplos aula1

---
+++ type-number.js
@@ -0,0 +1,25 @@
+//tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica)
+
+//pontos flutuantes
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const operacaoFlutuante = primeiroNumero / numeroPontoFlutuante;
+
+console.log(operacaoFlutuante)
+
+//NaN -> Not a Number (não é um número)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero)
---
+++ type-string.js
@@ -0,0 +1,18 @@
+const texto1 = "Ola Mundo";
+const texto2 = 'Ola, Mundo!';
+const senha = 'senhaSuperSegura456!';
+const stringDeNumeros = '123456';
+
+const citacao = "Meu nome é ";
+const meuNome = 'Vinicius'
+
+console.log(citacao)
+// tudo o que colocamos entre aspas simples ou duplas, o JS interpreta como String
+
+
+//template string OU template literal
+
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
