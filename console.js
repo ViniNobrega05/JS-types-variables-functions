927dc56e85c190d1c0f7f5db94259583cdcf75f6
ViniNobrega05 <vinicius_nobrega05@hotmail.com>
2023-06-06 11:43:00 -0300

    Aula 4 - erros e console

---
+++ console.js
@@ -0,0 +1,9 @@
+console.log() // log-> registro. podemos colocar nesse registro qlqr informacao (numero, string, variavel)
+const minhaVar = true;
+
+console.log (245)
+console.log ('eu sou um texto')
+console.log (minhaVar)
+
+console.error() // error -> tratamento de erro! Por exemplo os proprios erros que acusa no terminal quando se tem um erro na aplicacao
+console.error('deu erro!')
---
+++ erros.js
@@ -0,0 +1,9 @@
+// const numero; //isso causa um erro de syntax, de falta um inicializador na declaracao da constante (inicializador = valor declarado)
+const numero = 0; //solucao para o erro (atribuir um valor para a const)
+
+// console.log(numero // isso causa um erro de syntax, acusando a falta do ")" no caso
+console.log(numero) //solucao do caso acima
+
+//console.log(minhaVar) // isso causa um erro de referencia, pois a aplicacao nao encontrou a variavel, pois a mesma nao foi definida
+const minhaVar = 'oi'
+console.log(minhaVar1) // solucao do caso acima, primeiro se declara a variavel e dps é possivel utilizar
