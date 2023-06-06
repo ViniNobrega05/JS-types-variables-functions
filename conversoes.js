75c2eec586a5725e3d8db439c296d47786fc206a
ViniNobrega05 <vinicius_nobrega05@hotmail.com>
2023-06-06 10:01:35 -0300

    estudos e anotacoes gerais

--- conversoes.js
+++ conversoes.js
@@ -3,7 +3,7 @@
 
 //conversao implicita
 const numero = 456;
-const numeroString = '456a';
+const numeroString = '456';
 
 console.log(numero + numeroString)
 
---
+++ tipagem.js
@@ -0,0 +1,14 @@
+// tipagem dinamica - O JavaScript é uma linguagem do tipo dinamica, ou seja, permite a alteracao do tipo de dado de uma variavel, por exemplo:
+
+let minhaVar = 456;
+minhaVar='texto';
+minhaVar = true;
+
+// Alem disso, o JS é MULTIPARADIGMA, ou seja, o mesmo problema pode ser resolvido de diversas formas
+
+//ES6 ou ES2015 - versão do JS lancada em 2015, que permitiu melhorias e facilidades na programacao
+
+//o JS é uma Linguagem Interpretada, ou seja, nao precisa de compilador (Linguagem Compilada)
+
+//O NodeJS é um interpretador da linguagem JS. Podemos usar o Navegador, ou o Node. Quando usamos o Node, geralmente usamos no back-end, em servidores, por exemplo
+//É uma ferramenta para facilitar o trabalho na linguagem JS
